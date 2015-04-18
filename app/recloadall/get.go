cecc8cac4021b6acb2bcceeb0084e7e8fd20f487
Christopher John Jackson <hello@cj-jackson.com>
2015-04-18 09:40:40 +0100

    Improve robustness of updater

--- app/recloadall/get.go
+++ app/recloadall/get.go
@@ -1,9 +1,13 @@
 package recloadall
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 
 	"github.com/cjtoolkit/cfupdater/app/dfmt"
 	"github.com/cjtoolkit/cfupdater/app/settings"
@@ -23,7 +27,7 @@ func Get() (ipv4, ipv6 *Object) {
 
 	rec := &recloadall{}
 
-	err = json.NewDecoder(resp.Body).Decode(&rec)
+	err = json.NewDecoder(resp.Body).Decode(rec)
 	resp.Body.Close()
 
 	if err != nil {
@@ -71,4 +75,74 @@ func Get() (ipv4, ipv6 *Object) {
 	}
 
 	return
+}
+
+func (ob *Object) Run() {
+	url_ := "https://icanhazip.com/"
+	t := "ipv4"
+	if ob.Type == "AAAA" {
+		url_ = "https://ipv6.icanhazip.com/"
+		t = "ipv6"
+	}
+
+	buf := &bytes.Buffer{}
+	address := strings.TrimSpace(ob.Content)
+
+	dfmt.Println(address)
+
+	for {
+		tempaddress := ""
+		var _editRes *editRes
+		dfmt.Println("Getting IP Adddress (", t, ") from", url_)
+		resp, err := http.DefaultClient.Get(url_)
+		if err != nil {
+			dfmt.Println(url_, ": Timed out")
+			goto sleep
+		}
+		io.Copy(buf, resp.Body)
+		resp.Body.Close()
+		tempaddress = strings.TrimSpace(buf.String())
+		buf.Reset()
+		dfmt.Println("IP Adddres pull from", url_, ":", tempaddress)
+		if tempaddress == address {
+			dfmt.Println("Current IP (", t, ") is up-to-date!")
+			goto sleep
+		}
+
+		dfmt.Println("Updating IP Address (", t, ")!")
+		resp, err = http.DefaultClient.PostForm("https://www.cloudflare.com/api_json.html", url.Values{
+			"a":            {"rec_edit"},
+			"z":            {*settings.Z},
+			"type":         {ob.Type},
+			"id":           {ob.Id},
+			"name":         {ob.Name},
+			"content":      {tempaddress},
+			"ttl":          {ob.Ttl},
+			"service_mode": {ob.ServiceMode},
+			"email":        {*settings.Email},
+			"tkn":          {*settings.Tkn},
+		})
+
+		if err != nil {
+			dfmt.Println("https://www.cloudflare.com/api_json.html : Timed out (", t, ")")
+			goto sleep
+		}
+
+		err = json.NewDecoder(resp.Body).Decode(_editRes)
+		if err != nil {
+			dfmt.Println("CfUpdater json decoder failed (", t, ")")
+			goto sleep
+		}
+
+		_editRes = &editRes{}
+
+		if _editRes.Result != "success" {
+			dfmt.Println("CfUpdater failed to update IP address (", t, ")")
+			goto sleep
+		}
+
+		address = tempaddress
+	sleep:
+		time.Sleep(time.Duration(*settings.Hour) * time.Hour)
+	}
 }
--- app/recloadall/struct.go
+++ app/recloadall/struct.go
@@ -1,17 +1,5 @@
 package recloadall
 
-import (
-	"bytes"
-	"io"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
-
-	"github.com/cjtoolkit/cfupdater/app/dfmt"
-	"github.com/cjtoolkit/cfupdater/app/settings"
-)
-
 type recloadall struct {
 	Msg      *string   `json:"msg"`
 	Result   string    `json:"result"`
@@ -35,50 +23,6 @@ type Object struct {
 	ServiceMode string `json:"service_mode"`
 }
 
-func (ob *Object) Run() {
-	url_ := "https://icanhazip.com/"
-	t := "ipv4"
-	if ob.Type == "AAAA" {
-		url_ = "https://ipv6.icanhazip.com/"
-		t = "ipv6"
-	}
-
-	buf := &bytes.Buffer{}
-	address := strings.TrimSpace(ob.Content)
-
-	dfmt.Println(address)
-
-	for {
-		dfmt.Println("Getting IP Adddress (", t, ") from", url_)
-		resp, err := http.DefaultClient.Get(url_)
-		if err != nil {
-			dfmt.Println(url_, "Timed out")
-			time.Sleep(time.Duration(*settings.Hour) * time.Hour)
-			continue
-		}
-		io.Copy(buf, resp.Body)
-		tempaddress := strings.TrimSpace(buf.String())
-		dfmt.Println(tempaddress)
-		if tempaddress == address {
-			dfmt.Println("Current IP (", t, ") is up-to-date!")
-			goto sleep
-		}
-		address = tempaddress
-		dfmt.Println("Updating IP Address (", t, ")!")
-		http.DefaultClient.PostForm("https://www.cloudflare.com/api_json.html", url.Values{
-			"a":            {"rec_edit"},
-			"z":            {*settings.Z},
-			"type":         {ob.Type},
-			"id":           {ob.Id},
-			"name":         {ob.Name},
-			"content":      {address},
-			"ttl":          {ob.Ttl},
-			"service_mode": {ob.ServiceMode},
-			"email":        {*settings.Email},
-			"tkn":          {*settings.Tkn},
-		})
-	sleep:
-		buf.Reset()
-		time.Sleep(time.Duration(*settings.Hour) * time.Hour)
-	}
+type editRes struct {
+	Result string `json:"result"`
 }
