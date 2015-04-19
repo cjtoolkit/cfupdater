7c43e31e4e38d17d3c139da97fba0f88da0b5e03
Christopher John Jackson <hello@cj-jackson.com>
2015-04-19 10:50:01 +0100

    Added Comment

--- app/settings/settings.go
+++ app/settings/settings.go
@@ -5,15 +5,17 @@ import (
 )
 
 var (
+	// Mandatory
 	Tkn   = flag.String("tkn", "", "CloudFlare API Key")
 	Email = flag.String("email", "", "CloudFlare Email")
 	Z     = flag.String("z", "", "CloudFlare Zone")
 	Name  = flag.String("name", "", "Search Domain Name in Records")
-	Hour  = flag.Int64("hour", 2, "Run Every x Hours (2 Hours by Default)")
 
+	// Optional
 	IPv6    = flag.Bool("ipv6", false, "Enable IPv6 (AAAA) (Disabled by Default)")
 	Debug   = flag.Bool("debug", false, "Enable Debug Mode (Disabled by Default)")
 	Timeout = flag.Int64("timeout", 30, "Specify API Timeout in Second (Default: 30)")
+	Hour    = flag.Int64("hour", 2, "Run Every x Hours (2 Hours by Default)")
 )
 
 func init() {
