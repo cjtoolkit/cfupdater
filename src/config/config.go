28ef01e34c91476b7aa483dfa4d2d5eeaa2b6cf7
Christopher John Jackson <hello@cj-jackson.com>
2016-07-03 12:56:30 +0100

    Added config system.

---
+++ src/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const configPath = "~/.cfupdater/config.json"
+
+type Config struct {
+	// Mandatory
+	Email  string `json:"email"`
+	ApiKey string `json:"api_key"`
+	Zone   string `json:"zone"`
+	Name   string `json:"name"`
+
+	// Optional
+	Hour    int64 `json:"hour,omitempty"`
+	Timeout int64 `json:"timeout,omitempty"`
+	Debug   bool  `json:"debug,omitempty"`
+}
+
+var config = &Config{
+	Hour:    2,
+	Timeout: 30,
+}
+
+func InitConfig() error {
+	homePath := os.Getenv("HOME")
+
+	file, err := os.Open(homePath + configPath[1:])
+	if nil != err {
+		return err
+	}
+
+	return json.NewDecoder(file).Decode(config)
+}
+
+func GetConfig() Config {
+	return *config
+}
