9b6519af8fba00ae9fe95ee6ca41c284f0195c8d
Christopher John Jackson <hello@cj-jackson.com>
2016-07-03 12:39:04 +0100

    Simplify Ip Constructor

--- src/network/ip.go
+++ src/network/ip.go
@@ -9,8 +9,8 @@ import (
 )
 
 const (
-	ipv4_url = "https://ipv4.icanhazip.com/"
-	ipv6_url = "https://ipv6.icanhazip.com/"
+	Ipv4_url = "https://ipv4.icanhazip.com/"
+	Ipv6_url = "https://ipv6.icanhazip.com/"
 
 	http_client_timeout time.Duration = 5 * time.Second
 )
@@ -21,21 +21,13 @@ type Ip struct {
 	ipLookupUrl    string
 }
 
-func newIp(ipLookupUrl string) *Ip {
+func NewIp(ipLookupUrl string) *Ip {
 	return &Ip{
 		client: &http.Client{
 			Timeout: http_client_timeout,
 		},
 		ipLookupUrl: ipLookupUrl,
 	}
-}
-
-func NewIpV4() *Ip {
-	return newIp(ipv4_url)
-}
-
-func NewIpV6() *Ip {
-	return newIp(ipv6_url)
 }
 
 func (ip *Ip) FetchIpAddress() (address string, updated bool) {
