65976c95d98b382c34bd1bff5bce160cca68ff18
Christopher John Jackson <hello@cj-jackson.com>
2016-07-03 14:10:15 +0100

    Added url constants.

---
+++ src/cloudflare/urls.go
@@ -0,0 +1,29 @@
+package cloudflare
+
+import (
+	"net/http"
+	"strings"
+)
+
+const (
+	baseUrl = "https://api.cloudflare.com/client/v4"
+
+	zoneIdentifier = ":zone_identifier"
+	identifier     = ":identifier"
+
+	listZones       = baseUrl + "/zones"
+	listDnsRecords  = listZones + "/" + zoneIdentifier + "/dns_records"
+	updateDnsRecord = listDnsRecords + "/" + identifier
+
+	listZonesMethod      = http.MethodGet
+	listDnsRecordsMethod = http.MethodGet
+	updateDnsRecord      = http.MethodPut
+)
+
+func urlSearchReplace(rawUrl string, urlData map[string]string) string {
+	for key, value := range urlData {
+		rawUrl = strings.Replace(rawUrl, key, value, 1)
+	}
+
+	return rawUrl
+}
